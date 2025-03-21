44a0b4a0e8faffff06efad5e1d0ff13124d725dd
chamupathi <chamupathi2008@gmail.com>
2025-03-21 23:53:57 +0000

    add why choose us

---
+++ src/app/ui/config/navigation.ts
@@ -0,0 +1,14 @@
+export const navigationLinks = [
+    {
+        label: "Home",
+        href: "/"
+    },
+    {
+        label: "Why Choose Us",
+        href: "/why-choose-us/"
+    },
+    {
+        label: "Sign Up",
+        href: "/sign-up"
+    }
+]; 
---
+++ src/app/why-choose-us/page.tsx
@@ -0,0 +1,7 @@
+export default function WhyChooseUs() {
+    return (
+        <div>
+            <h1>Why Choose Us</h1>
+        </div>
+    )
+}
