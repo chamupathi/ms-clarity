5d55836c5ba85f19019abd3e9046b72c58328044
chamupathi <chamupathi2008@gmail.com>
2025-03-22 15:09:37 +0000

    move clarity init to own component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import BaseLayout from "./ui/layouts/BaseLayout";
+import ClarityInit from "./ui/analytics/clarity-init";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +16,11 @@ export default function RootLayout({
 }: {
     children: React.ReactNode;
 }) {
+    
     return (
         <html lang="en">
             <body className={inter.className}>
+                <ClarityInit />
                 <BaseLayout>
                     {children}
                 </BaseLayout>
--- src/app/ui/layouts/BaseLayout.tsx
+++ src/app/ui/layouts/BaseLayout.tsx
@@ -3,8 +3,8 @@
 import Image from "next/image";
 import Header from "../header";
 import SideNavigation from "../SideNavigation";
-import { useEffect, useState } from "react";
-import Clarity from '@microsoft/clarity';
+import { useState } from "react";
+
 
 interface BaseLayoutProps {
     children: React.ReactNode;
@@ -13,37 +13,6 @@ interface BaseLayoutProps {
 export default function BaseLayout({ children }: BaseLayoutProps) {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    // useEffect(() => {
-    //     (function(c, l, a, r, i, t, y) {
-    //       c[i] = c[i] || function() { (c[i].q = c[i].q || []).push(arguments) };
-    //       t = l.createElement(a); t.async = 1; t.src = r;
-    //       y = l.getElementsByTagName(a)[0]; y.parentNode.insertBefore(t, y);
-    //     })(window, document, "script", "https://www.clarity.ms/tag/qrxizr0930", "clarity");
-    //   }, []);
-
-    
-      /* eslint-disable */
-      useEffect(() => {
-        // if (typeof window !== "undefined") {
-        //   (function (c: any, l: Document, a: string, r: string, i: string, t?: HTMLScriptElement, y?: Node) {
-        //     c[i] =
-        //       c[i] ||
-        //       function (...args: any) {
-        //         (c[i].q = c[i].q || []).push(args);
-        //       };
-        //     t = l.createElement(a) as HTMLScriptElement;
-        //     t.async = true;
-        //     t.src = r;
-        //     y = l.getElementsByTagName(a)[0];
-        //     y?.parentNode?.insertBefore(t, y);
-        //   })(window, document, "script", "https://www.clarity.ms/tag/qrxizr0930", "clarity");
-        // }
-
-        if (typeof window !== "undefined") {
-            Clarity.init('qrxizr0930');
-        }
-      }, []);
-      /* eslint-enable */
 
     return (
         <>
