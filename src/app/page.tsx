0d13d302b5f0dafc5eae79b15c5e10c4be604566
chamupathi <chamupathi2008@gmail.com>
2025-03-22 00:09:11 +0000

    change base layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import SearchForm from "./ui/components/SearchForm";
 
 export default function Home() {
   return (
-    <div className="max-w-screen-xl mx-auto ">
-      <div className="p-6 pt-35">
+    <div className=" ">
+      <div className="max-w-screen-xl mx-auto p-6 pt-35">
         <MainBanner />
         <SearchForm />
 
 
       </div>
 
-      <div className="flex flex-col md:flex-row gap-4 bg-white m-0 p-12 pt-30 pb-30 items-center">
+      <div className="flex flex-col md:flex-row bg-white m-0 p-0 pt-30 pb-30 items-center">
         <div className="w-full md:max-w-md md:mx-auto">
           <h6 className="text-xl font-bold text-center italic">&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.&quot;</h6>
           <h6 className="text-lg text-center mt-4 uppercase">&quot;John Doe&quot;</h6>
--- src/app/ui/layouts/BaseLayout.tsx
+++ src/app/ui/layouts/BaseLayout.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Header from "../header";
 import SideNavigation from "../SideNavigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface BaseLayoutProps {
     children: React.ReactNode;
@@ -11,6 +11,34 @@ interface BaseLayoutProps {
 
 export default function BaseLayout({ children }: BaseLayoutProps) {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // useEffect(() => {
+    //     (function(c, l, a, r, i, t, y) {
+    //       c[i] = c[i] || function() { (c[i].q = c[i].q || []).push(arguments) };
+    //       t = l.createElement(a); t.async = 1; t.src = r;
+    //       y = l.getElementsByTagName(a)[0]; y.parentNode.insertBefore(t, y);
+    //     })(window, document, "script", "https://www.clarity.ms/tag/qrxizr0930", "clarity");
+    //   }, []);
+
+    
+      /* eslint-disable */
+      useEffect(() => {
+        if (typeof window !== "undefined") {
+          (function (c: any, l: Document, a: string, r: string, i: string, t?: HTMLScriptElement, y?: Node) {
+            c[i] =
+              c[i] ||
+              function (...args: any) {
+                (c[i].q = c[i].q || []).push(args);
+              };
+            t = l.createElement(a) as HTMLScriptElement;
+            t.async = true;
+            t.src = r;
+            y = l.getElementsByTagName(a)[0];
+            y?.parentNode?.insertBefore(t, y);
+          })(window, document, "script", "https://www.clarity.ms/tag/qrxizr0930", "clarity");
+        }
+      }, []);
+      /* eslint-enable */
 
     return (
         <>
